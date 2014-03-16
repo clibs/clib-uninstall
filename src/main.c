e8a89519bf120d48f0af17f0ad5851d46211c667
Stephen Mathieson <smath23@gmail.com>
2014-03-15 20:56:35 -0400

    main: Use parse-repo

--- src/main.c
+++ src/main.c
@@ -9,62 +9,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "commander/commander.h"
-#include "substr/substr.h"
-#include "str-copy/str-copy.h"
+#include "parse-repo/parse-repo.h"
 #include "clib-uninstall.h"
 #include "log.h"
 
-// TODO replace
-static char *
-parse_author(const char *slug) {
-  char *name = NULL;
-  char *author = NULL;
-  char *copy = str_copy(slug);
-  if (NULL == copy) return NULL;
-
-  name = strstr(copy, "/");
-  if (!name) {
-    author = str_copy("clibs");
-    goto cleanup;
-  }
-
-  int delta = name - copy;
-  if (!delta || !(author = malloc(delta * sizeof(char *)))) goto cleanup;
-  author = substr(copy, 0, delta);
-
-cleanup:
-  free(copy);
-  return author;
-}
-
-// TODO replace
-static char *
-parse_name(const char *slug) {
-  char *copy = NULL;
-  char *name = NULL;
-  char *t = NULL;
-  char *version = NULL;
-
-  if (!(copy = str_copy(slug))) return NULL;
-  version = strstr(copy, "@");
-  if (version) {
-    // drop version
-    copy = substr(copy, 0, version - copy);
-  }
-  t = strstr(copy, "/");
-  if (!t) {
-    name = copy;
-    goto cleanup;
-  }
-  t++;
-  if (0 == strlen(t)) goto cleanup;
-  name = t;
-
-cleanup:
-  free(copy);
-  return str_copy(name);
-}
-
+#ifndef DEFAULT_REPO_OWNER
+#define DEFAULT_REPO_OWNER "clibs"
+#endif
 
 int
 main(int argc, char **argv) {
@@ -80,9 +31,9 @@ main(int argc, char **argv) {
   if (0 == program.argc) command_help(&program);
 
   for (int i = 0; i < program.argc; i++) {
-    char *owner = parse_author(program.argv[i]);
+    char *owner = parse_repo_owner(program.argv[i], DEFAULT_REPO_OWNER);
     if (!owner) goto cleanup;
-    char *name = parse_name(program.argv[i]);
+    char *name = parse_repo_name(program.argv[i]);
     if (!name) {
       free(owner);
       goto cleanup;
